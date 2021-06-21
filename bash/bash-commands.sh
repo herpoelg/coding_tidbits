b3e658324e7107189775f44c732b878719766857
gherpoel <g.herpoelaert@gmail.com>
2021-06-21 21:33:31 +0200

    refreshing bash basics

---
+++ bash/bash-commands.sh
@@ -0,0 +1,13 @@
+#convenient bash commands
+#changing the prompt to just a dollar sign
+sh1='$'
+
+#curl
+#curl is a tool to transfer data from or to a server, using one of the supported protocols (HTTP, HTTPS, FTP, FTPS, TFTP, DICT, TELNET, LDAP or FILE). The command is designed to work without user interaction.
+curl -o memorials-of-old-lincolnshire.txt http://www.het.brown.edu/guide/UNIX-password-security.txt
+
+#wget
+GNU Wget is a free utility for non-interactive download of files from the Web. It supports HTTP, HTTPS, and FTP protocols, as well as retrieval through HTTP proxies.
+wget -O memorials-of-old-lincolnshire.txt https://www.gutenberg.org/files/65653/65653-0.txt
+
+#to read: http://linuxcommand.org/
--- python/parquet-columnar-storage.py
+++ python/parquet-columnar-storage.py
@@ -1,9 +1,8 @@
-#exploration based on https://blog.datasyndrome.com/python-and-parquet-performance-e71da65269ce
+#exploration based on https://arrow.apache.org/docs/python/parquet.html#reading-and-writing-single-files
 
 #Parquet format is optimized in three main ways: columnar storage, columnar compression and data partitioning.
 
 
-#https://arrow.apache.org/docs/python/parquet.html
 import pyarrow.parquet as pq
 
 import numpy as np
@@ -19,4 +18,14 @@ df = pd.DataFrame({'one': [-1, np.nan, 2.5],
 
 
 table = pa.Table.from_pandas(df)
-pq.write_table(table, 'example.parquet')
+pq.write_table(table, 'example.parquet') #creating a single parquet file
+
+#reading it back in
+table2 = pq.read_table('example.parquet')
+
+df2 = table2.to_pandas()
+print(type(df2))
+print(df2.head())
+
+#reading in a subset of columns
+pq.read_table('example.parquet', columns=['one', 'three'])
